5d5e6a88f46505a47e4f59e73943093f2729a00b
Alessandro Minotti <aethelfirt86@gmail.com>
2018-02-06 19:01:12 +0100

    Added more trees to root files with reconstructed alhpas, topologies, and time informations

--- mc_test/Module_BiPo/BiPo.cc
+++ mc_test/Module_BiPo/BiPo.cc
@@ -46,33 +46,57 @@ void BiPo::initialize(const datatools::properties & setup_,
     _root_file_ = new TFile(root_filename.c_str(), "RECREATE");
     _root_file_->cd();
     
-    _root_tree_simulated_electrons_ = new TTree("tree_se", "tree_se");
+    _root_tree_simulated_electrons_ = new TTree("tree_mc_elec", "tree_mc_elec");
     _root_tree_simulated_electrons_->SetDirectory(_root_file_);
     _root_tree_simulated_electrons_->Branch("sd.primary_electrons_kinetic_energy", &_root_variables_simulated_particles_.kinetic_energy);
     _root_tree_simulated_electrons_->Branch("sd.primary_electrons_total_energy", &_root_variables_simulated_particles_.total_energy);
-
+    
+    _root_tree_simulated_alphas_ = new TTree("tree_mc_alpha", "tree_mc_alpha");
+    _root_tree_simulated_alphas_->SetDirectory(_root_file_);
+    _root_tree_simulated_alphas_->Branch("sd.primary_alphas_kinetic_energy", &_root_variables_simulated_particles_.kinetic_energy);
+    _root_tree_simulated_alphas_->Branch("sd.primary_alphas_total_energy", &_root_variables_simulated_particles_.total_energy);
+    _root_tree_simulated_alphas_->Branch("sd.primary_alphas_time", &_root_variables_simulated_particles_.emission_time);
+    
     _root_tree_reconstructed_electrons_source_sel_ = new TTree("tree_rec_elec_source", "tree_rec_elec_source");
     _root_tree_reconstructed_electrons_source_sel_->SetDirectory(_root_file_);
-    _root_tree_reconstructed_electrons_source_sel_->Branch("sd.reconstructed_electrons_kinetic_energy", &_root_variables_simulated_particles_.kinetic_energy);
+    //_root_tree_reconstructed_electrons_source_sel_->Branch("sd.reconstructed_electrons_kinetic_energy", &_root_variables_simulated_particles_.kinetic_energy);
     _root_tree_reconstructed_electrons_source_sel_->Branch("ptd.reconstructed_electrons_n_geiger_hits", &_root_variables_reconstructed_particles_.n_geiger_hits);
     _root_tree_reconstructed_electrons_source_sel_->Branch("ptd.reconstructed_electrons_track_length", &_root_variables_reconstructed_particles_.track_length);
     
     _root_tree_reconstructed_electrons_tracker_sel_ = new TTree("tree_rec_elec_tracker", "tree_rec_elec_tracker");
     _root_tree_reconstructed_electrons_tracker_sel_->SetDirectory(_root_file_);
-    _root_tree_reconstructed_electrons_tracker_sel_->Branch("sd.reconstructed_electrons_kinetic_energy", &_root_variables_simulated_particles_.kinetic_energy);
+    //_root_tree_reconstructed_electrons_tracker_sel_->Branch("sd.reconstructed_electrons_kinetic_energy", &_root_variables_simulated_particles_.kinetic_energy);
     _root_tree_reconstructed_electrons_tracker_sel_->Branch("ptd.reconstructed_electrons_n_geiger_hits", &_root_variables_reconstructed_particles_.n_geiger_hits);
     _root_tree_reconstructed_electrons_tracker_sel_->Branch("ptd.reconstructed_electrons_track_length", &_root_variables_reconstructed_particles_.track_length);
-
-    _root_tree_reconstructed_alphas_source_sel_ = new TTree("tree_alpha_source", "tree_alpha_source");
+    
+    _root_tree_reconstructed_alphas_source_sel_ = new TTree("tree_rec_alpha_source", "tree_rec_alpha_source");
     _root_tree_reconstructed_alphas_source_sel_->SetDirectory(_root_file_);
     _root_tree_reconstructed_alphas_source_sel_->Branch("ptd.reconstructed_alphas_n_geiger_hits", &_root_variables_reconstructed_particles_.n_geiger_hits);
     _root_tree_reconstructed_alphas_source_sel_->Branch("ptd.reconstructed_alphas_track_length", &_root_variables_reconstructed_particles_.track_length);
     
-    _root_tree_reconstructed_alphas_tracker_sel_ = new TTree("tree_alpha_tracker", "tree_alpha_tracker");
+    _root_tree_reconstructed_alphas_tracker_sel_ = new TTree("tree_rec_alpha_tracker", "tree_rec_alpha_tracker");
     _root_tree_reconstructed_alphas_tracker_sel_->SetDirectory(_root_file_);
     _root_tree_reconstructed_alphas_tracker_sel_->Branch("ptd.reconstructed_alphas_n_geiger_hits", &_root_variables_reconstructed_particles_.n_geiger_hits);
     _root_tree_reconstructed_alphas_tracker_sel_->Branch("ptd.reconstructed_alphas_track_length", &_root_variables_reconstructed_particles_.track_length);
-
+    
+    _root_tree_fitted_alphas_ = new TTree("tree_fit_alpha", "tree_fit_alpha");
+    _root_tree_fitted_alphas_->SetDirectory(_root_file_);
+    _root_tree_fitted_alphas_->Branch("ptd.reconstructed_alphas_n_geiger_hits", &_root_variables_reconstructed_particles_.n_geiger_hits);
+    _root_tree_fitted_alphas_->Branch("ptd.reconstructed_alphas_track_length", &_root_variables_reconstructed_particles_.track_length);
+    _root_tree_fitted_alphas_->Branch("ptd.reconstructed_alphas_delta_t",
+       &_root_variables_reconstructed_particles_.delta_t_true_fit);
+    
+    _root_tree_reconstructed_1e1a_topology_source_sel_ = new TTree("tree_rec_1e1a_source", "tree_rec_1e1a_source");
+    _root_tree_reconstructed_1e1a_topology_source_sel_->SetDirectory(_root_file_);
+    _root_tree_reconstructed_1e1a_topology_source_sel_->Branch("ptd.reconstructed_alphas_track_length", &_root_variables_topologies_.alpha_track_length);
+    _root_tree_reconstructed_1e1a_topology_source_sel_->Branch("ptd.reconstructed_1e1a_delta_t", &_root_variables_topologies_.delta_t_prompt_delayed);
+    
+    _root_tree_reconstructed_1e1a_topology_tracker_sel_ = new TTree("tree_rec_1e1a_tracker", "tree_rec_1e1a_tracker");
+    _root_tree_reconstructed_1e1a_topology_tracker_sel_->SetDirectory(_root_file_);
+    _root_tree_reconstructed_1e1a_topology_tracker_sel_->Branch("ptd.reconstructed_alphas_track_length", &_root_variables_topologies_.alpha_track_length);
+    _root_tree_reconstructed_1e1a_topology_tracker_sel_->Branch("ptd.reconstructed_1e1a_delta_t", &_root_variables_topologies_.delta_t_prompt_delayed);
+    
+    
     // Counters    
     _number_of_simulated_electrons_ = 0;
     _number_of_simulated_alphas_ = 0;
@@ -107,7 +131,11 @@ dpp::base_module::process_status BiPo::process(datatools::things & record_) {
   _number_of_event_alphas_ = 0;
   _number_of_event_gammas_ = 0;
     
-    
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "---------   NEW EVENT   ---------" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+
     ///////////////////////////////////////////
     ////////////  MONTE CARLO DATA  ///////////
     ///////////////////////////////////////////
@@ -127,22 +155,31 @@ dpp::base_module::process_status BiPo::process(datatools::things & record_) {
         
         my_primary_particle[i_particle] = my_primary_event.get_particle(i_particle);
         
+        _root_variables_simulated_particles_.kinetic_energy = my_primary_particle[i_particle].get_kinetic_energy();
+        _root_variables_simulated_particles_.total_energy = my_primary_particle[i_particle].get_total_energy();
+        _root_variables_simulated_particles_.emission_time = my_primary_particle[i_particle].get_time();
+
         if(my_primary_particle[i_particle].is_electron()) {
 
 	      _number_of_event_electrons_ ++;                
 	      _number_of_simulated_electrons_ ++;
-              
-	      _root_variables_simulated_particles_.kinetic_energy = my_primary_particle[i_particle].get_kinetic_energy();
-	      _root_variables_reconstructed_particles_.kinetic_energy = my_primary_particle[i_particle].get_kinetic_energy();
-	      _root_variables_simulated_particles_.total_energy = my_primary_particle[i_particle].get_total_energy();
-	      _root_tree_simulated_electrons_->Fill();
-              
+            
+          electron_true_ekin = _root_variables_simulated_particles_.kinetic_energy;
+          electron_true_t = _root_variables_simulated_particles_.emission_time;
+	      
+          _root_tree_simulated_electrons_->Fill();
+            
+          //_root_variables_reconstructed_particles_.kinetic_energy = my_primary_particle[i_particle].get_kinetic_energy();
         }
         
 	    else if(my_primary_particle[i_particle].is_alpha()) {
 
 	      _number_of_event_alphas_ ++;
 	      _number_of_simulated_alphas_ ++;
+            
+          alpha_true_t = _root_variables_simulated_particles_.emission_time;
+            
+          _root_tree_simulated_alphas_->Fill();
 	    }
         
 	    else if(my_primary_particle[i_particle].is_gamma()) {
@@ -157,23 +194,29 @@ dpp::base_module::process_status BiPo::process(datatools::things & record_) {
     //////////  RECONSTRUCTION DATA  //////////
     ///////////////////////////////////////////
     
-    //////////  Condition to select 1e1a events  //////////
-    if (_number_of_event_electrons_ != 1 || _number_of_event_alphas_ != 1) {
-        return PROCESS_CONTINUE;
-    }
-    _number_of_simulated_1e1a_ ++; //increment counter of searched topology
-
     if (!record_.has("PTD"))
         return PROCESS_CONTINUE;
     
     const snemo::datamodel::particle_track_data & my_ptd = record_.get<snemo::datamodel::particle_track_data>("PTD");
         
-    if (!my_ptd.has_particles())
+    if (!my_ptd.has_particles() || my_ptd.has_non_associated_calorimeters())
         return PROCESS_CONTINUE;
-        
+    
+    //////////  Condition to select 1e1a events  //////////
+    if (_number_of_event_electrons_ != 1 || _number_of_event_alphas_ != 1) {
+        return PROCESS_CONTINUE;
+    }
+    _number_of_simulated_1e1a_ ++; //increment counter of searched topology
+    
+    
     //////////  LOOP OVER PARTICLES  //////////
     const snemo::datamodel::particle_track_data::particle_collection_type & my_particles = my_ptd.get_particles();
-        
+    
+    _got_alpha_source_sel_ = false;
+    _got_electron_source_sel_ = false;
+    _got_alpha_tracker_sel_ = false;
+    _got_electron_tracker_sel_ = false;
+    
     for (const auto & ip : my_particles) {
         const snemo::datamodel::particle_track & my_pt = ip.get();
         
@@ -183,6 +226,7 @@ dpp::base_module::process_status BiPo::process(datatools::things & record_) {
         is_delayed = false;
         does_touch_foil = false;
         does_touch_wall = false;
+        does_touch_calo = false;
         has_negative_charge = false;
         has_no_charge = false;
         has_positive_charge = false;
@@ -202,13 +246,23 @@ dpp::base_module::process_status BiPo::process(datatools::things & record_) {
         //////////  STUDY TRAJECTORY  //////////
         snemo::datamodel::tracker_trajectory my_tj = my_pt.get_trajectory();
         
-        if(my_tj.get_pattern().get_pattern_id() == "helix")
+        if(my_tj.get_pattern().get_pattern_id() == "helix") {
             is_helix = true;
-        else if(my_tj.get_pattern().get_pattern_id() == "line")
+            
+            _root_variables_reconstructed_particles_.fitted_time = my_tj.get_auxiliaries().fetch_real_scalar("t0");
+            electron_fitted_t = _root_variables_reconstructed_particles_.fitted_time;
+        }
+        else if(my_tj.get_pattern().get_pattern_id() == "line") {
             is_straight = true;
 	
+        //if(my_tj.has_auxiliaries())
+            _root_variables_reconstructed_particles_.fitted_time = my_tj.get_auxiliaries().fetch_real_scalar("t0");
+            alpha_fitted_t = _root_variables_reconstructed_particles_.fitted_time;
+            _root_variables_topologies_.alpha_track_length = my_tj.get_pattern().get_shape().get_length();
+        }
         _root_variables_reconstructed_particles_.track_length = my_tj.get_pattern().get_shape().get_length();
 
+        
         if(my_tj.has_cluster()) {
           snemo::datamodel::tracker_cluster my_cl = my_tj.get_cluster();
           _root_variables_reconstructed_particles_.n_geiger_hits = my_cl.get_number_of_hits();
@@ -227,6 +281,11 @@ dpp::base_module::process_status BiPo::process(datatools::things & record_) {
                     
             if(snemo::datamodel::particle_track::vertex_is (iv.get(), snemo::datamodel::particle_track::vertex_type::VERTEX_ON_MAIN_CALORIMETER))
                 does_touch_wall = true;
+            
+            if(snemo::datamodel::particle_track::vertex_is (iv.get(), snemo::datamodel::particle_track::vertex_type::VERTEX_ON_MAIN_CALORIMETER)
+               ||snemo::datamodel::particle_track::vertex_is (iv.get(), snemo::datamodel::particle_track::vertex_type::VERTEX_ON_X_CALORIMETER)
+               ||snemo::datamodel::particle_track::vertex_is (iv.get(), snemo::datamodel::particle_track::vertex_type::VERTEX_ON_GAMMA_VETO) )
+                does_touch_calo = true;
         
         } /// end loop over vertices ///
 
@@ -242,12 +301,22 @@ dpp::base_module::process_status BiPo::process(datatools::things & record_) {
             }
         }*/
         
+        if(is_delayed && is_straight && has_no_charge) {
+            
+            if(alpha_fitted_t != 0 && alpha_true_t !=0)
+                _root_variables_reconstructed_particles_.delta_t_true_fit = alpha_true_t - alpha_fitted_t;
+            else _root_variables_reconstructed_particles_.delta_t_true_fit = pow(-1,0.5);
+            _root_tree_fitted_alphas_->Fill();
+        }
+        
         //////////  CUT FLOW: SOURCE SEL  //////////
         if(is_helix) { _number_of_electrons_++;
             if(does_touch_foil) { _number_of_foil_electrons_++;
                 if(does_touch_wall) { _number_of_wall_electrons_++;
                     if(has_negative_charge) {
+                        _got_electron_source_sel_ = true;
                         _number_of_negative_charge_electrons_++;
+                        //t_electron_source_sel = 0;
                         _root_tree_reconstructed_electrons_source_sel_->Fill();
                     }
                 }
@@ -257,33 +326,68 @@ dpp::base_module::process_status BiPo::process(datatools::things & record_) {
             if(is_delayed) {_number_of_delayed_alphas_++;
                 if(does_touch_foil) {_number_of_foil_alphas_++;
                     if(!does_touch_wall) {
+                        _got_alpha_source_sel_ = true;
                         _number_of_nowall_alphas_++;
+                        //t_alpha_source_sel = 0;
                         _root_tree_reconstructed_alphas_source_sel_->Fill();
                     }
                 }
             }
         }
+        if(_got_electron_source_sel_ && _got_alpha_source_sel_) {
+            
+            _root_variables_topologies_.alpha_track_length = alpha_track_length;
+            _root_variables_topologies_.delta_t_prompt_delayed = alpha_fitted_t - electron_fitted_t;
+            _root_tree_reconstructed_1e1a_topology_source_sel_->Fill();
+        }
+        
         //////////  CUT FLOW: TRACKER SEL  //////////
         if(is_prompt) { _number_of_prompt_electrons_++;
-            if(does_touch_wall) { _number_of_anywall_electrons_++; //check that is ANY wall
-                _root_tree_reconstructed_electrons_tracker_sel_->Fill();
+            if(does_touch_wall) {
+                _got_electron_tracker_sel_ = true;
+                if(does_touch_calo) {
+                    _number_of_anywall_electrons_++;
+                    //t_electron_tracker_sel = 0;
+                    _root_tree_reconstructed_electrons_tracker_sel_->Fill();
+                }
             }
         }
         if(is_delayed) {
             if(is_delayed) {
                 if(!does_touch_wall) {
                     if(!does_touch_foil) {
+                        _got_alpha_tracker_sel_ = true;
                         _number_of_nofoil_alphas_++;
+                        //t_alpha_tracker_sel = 0;
                         _root_tree_reconstructed_alphas_tracker_sel_->Fill();
                     }
                 }
             }
         }
-        
+        if(_got_electron_tracker_sel_ && _got_alpha_tracker_sel_) {
+            
+            _root_variables_topologies_.alpha_track_length = alpha_track_length;
+            _root_variables_topologies_.delta_t_prompt_delayed = alpha_fitted_t - electron_fitted_t;
+            _root_tree_reconstructed_1e1a_topology_tracker_sel_->Fill();
+        }
 
 
-        
     } /// end loop over particles ///
+    
+    
+    if(_got_electron_source_sel_ && _got_alpha_source_sel_) {
+        
+        
+        
+        
+    }
+    
+    if(_got_electron_tracker_sel_ && _got_alpha_tracker_sel_) {
+        
+        
+        
+        
+    }
 
     return PROCESS_OK;
 }
@@ -294,10 +398,14 @@ void BiPo::reset() {
         // write the output, finished streaming
         _root_file_->cd();
         _root_tree_simulated_electrons_->Write();
+        _root_tree_simulated_alphas_->Write();
         _root_tree_reconstructed_electrons_source_sel_->Write();
         _root_tree_reconstructed_electrons_tracker_sel_->Write();
         _root_tree_reconstructed_alphas_source_sel_->Write();
         _root_tree_reconstructed_alphas_tracker_sel_->Write();
+        _root_tree_fitted_alphas_->Write();
+        _root_tree_reconstructed_1e1a_topology_source_sel_->Write();
+        _root_tree_reconstructed_1e1a_topology_tracker_sel_->Write();
         _root_file_->Close();
         // clean up
         delete _root_file_;
@@ -331,7 +439,7 @@ void BiPo::print_results() {
       " - (" << 100*(double)_number_of_wall_electrons_/(double)_number_of_simulated_1e1a_<<
       " +/- " << 100*pow((double)_number_of_wall_electrons_,0.5)/(double)_number_of_simulated_1e1a_<<
       ")% of total simulated electrons" << std::endl;
-    std::cout << "Number of electrons with negatie charge: " << _number_of_negative_charge_electrons_ <<
+    std::cout << "Number of electrons with negative charge: " << _number_of_negative_charge_electrons_ <<
       " - (" << 100*(double)_number_of_negative_charge_electrons_/(double)_number_of_simulated_1e1a_<<
       " +/- " << 100*pow((double)_number_of_negative_charge_electrons_,0.5)/(double)_number_of_simulated_1e1a_<<
       ")% of total simulated electrons" << std::endl;
@@ -355,7 +463,7 @@ void BiPo::print_results() {
     
     std::cout << "------  RESULTS OF THE CUT FLOW FOR TRACKER SELECTION  ------" << std::endl;
 
-    std::cout << "Number of electrons that are delayed: " << _number_of_prompt_electrons_ <<
+    std::cout << "Number of electrons that are prompt: " << _number_of_prompt_electrons_ <<
     " - (" << 100*(double)_number_of_prompt_electrons_/(double)_number_of_simulated_1e1a_<<
     " +/- " << 100*pow((double)_number_of_prompt_electrons_,0.5)/(double)_number_of_simulated_1e1a_<<
     ")% of total simulated electrons" << std::endl;
--- mc_test/Module_BiPo/BiPo.h
+++ mc_test/Module_BiPo/BiPo.h
@@ -20,11 +20,23 @@ class TH1I;
 class TH1F;
 
 // Structure to embed ROOT variable
-struct root_variables {
+struct root_variables_particles {
+  //SD variables
   double kinetic_energy;
   double total_energy;
+  double emission_time;
+  //PTD variables
   int n_geiger_hits;
   double track_length;
+  double fitted_time;
+  double delta_t_true_fit;
+};
+struct root_variables_topologies {
+  //int n_particles;
+  //bool has_prompt;
+  //bool has_delayed;
+  double alpha_track_length;
+  double delta_t_prompt_delayed;
 };
 
 class BiPo : public dpp::base_module {
@@ -47,13 +59,19 @@ private:
 
     TFile * _root_file_;
     TTree * _root_tree_simulated_electrons_;
+    TTree * _root_tree_simulated_alphas_;
     TTree * _root_tree_reconstructed_electrons_source_sel_;
     TTree * _root_tree_reconstructed_electrons_tracker_sel_;
     TTree * _root_tree_reconstructed_alphas_source_sel_;
     TTree * _root_tree_reconstructed_alphas_tracker_sel_;
+    TTree * _root_tree_fitted_alphas_;
+    TTree * _root_tree_reconstructed_1e1a_topology_source_sel_;
+    TTree * _root_tree_reconstructed_1e1a_topology_tracker_sel_;
 
-    root_variables _root_variables_simulated_particles_, _root_variables_reconstructed_particles_;
-    //root_histograms _root_histograms_;
+    root_variables_particles _root_variables_simulated_particles_;
+    root_variables_particles _root_variables_reconstructed_particles_;
+    
+    root_variables_topologies _root_variables_topologies_;
     
     bool is_helix;
     bool is_straight;
@@ -61,9 +79,15 @@ private:
     bool is_delayed;
     bool does_touch_foil;
     bool does_touch_wall;
+    bool does_touch_calo;
     bool has_negative_charge;
     bool has_no_charge;
     bool has_positive_charge;
+    
+    bool _got_alpha_source_sel_;
+    bool _got_electron_source_sel_;
+    bool _got_alpha_tracker_sel_;
+    bool _got_electron_tracker_sel_;
 
     unsigned int _number_of_simulated_electrons_;
     unsigned int _number_of_simulated_alphas_;
@@ -89,6 +113,10 @@ private:
     unsigned int _number_of_nofoil_alphas_;
 
     unsigned int n_wierdos;
+    
+    //Temporary variables
+    double alpha_true_t, alpha_fitted_t, electron_true_t, electron_fitted_t;
+    double electron_true_ekin;
 
     DPP_MODULE_REGISTRATION_INTERFACE(BiPo)
 
