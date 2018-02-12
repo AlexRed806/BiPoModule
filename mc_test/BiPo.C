573b9efed13bac98fd899a62d1dbba6983922ffc
Alessandro Minotti <aethelfirt86@gmail.com>
2018-02-12 18:40:13 +0100

    BiPo.C updated for new 200000 events root files

--- mc_test/BiPo.C
+++ mc_test/BiPo.C
@@ -22,40 +22,45 @@ void BiPo() {
     gStyle->SetOptStat(1111111);
     //gStyle->SetOptStat(0);
 
+    const int days_of_exposure = 7;
     
     const unsigned int n_files = 3;
-    const int n_events = 100000;
+    const int n_events = 200000;
     const double activities[n_files] = {0.0154,0.00018,0.0455};
-    //const double efficiencies[n_files] = {0.018342, 0.0866779, 0.0080547};
-    const double efficiencies[n_files] = {0.109888, 0.00555601, 0.0897256};
-    const double given_exposure = 86400.*60.;
-    const int n_pseudo = 10000;
+    const double efficiencies[n_files] = {0.018342, 0.0866779, 0.0080547};
+    //const double efficiencies[n_files] = {0.109888, 0.00555601, 0.0897256};
+    const double given_exposure = 86400.*(double)days_of_exposure;
+    const int n_pseudo = 1000;
     
     double mc_exposures[n_files];
     for(int jj=0;jj<3;jj++) mc_exposures[jj] = (double)n_events / activities[jj];
- 
+    
     char name[56];
     char simul_names[3][54] = {"source_bulk","source_surface","tracker_all"};
 
     TH1D *h_track_length[n_files];
     for(int i_file=0; i_file<n_files; i_file++) {
         sprintf(name,"track_length_%s",simul_names[i_file]);
-        h_track_length[i_file] = new TH1D(name,name,50,0,500);
+        h_track_length[i_file] = new TH1D(name,name,100,0,500);
     }
-    TH1D *h_cumulative_track_length = new TH1D("cumulative_track_length","cumulative_track_length",50,0,500);
+    TH1D *h_cumulative_track_length = new TH1D("cumulative_track_length","cumulative_track_length",100,0,500);
 
-    TH1D *h_pseudo_track_length = new TH1D("pseudo_track_length","pseudo_track_length",50,0,500);
+    TH1D *h_pseudo_track_length = new TH1D("pseudo_track_length","pseudo_track_length",100,0,500);
 
     for(int i_file=0; i_file<n_files; i_file++) {
     
         stringstream simul_name;
-        simul_name << "./root_files/BiPo_100000ev/BiPo_" << simul_names[i_file] << "_100000ev.root";
+        simul_name << "./root_files/BiPo_200000ev/BiPo_" << simul_names[i_file] << "_200000ev_merged.root";
         cout << "Opening file " << simul_name.str() << endl;
  
         TFile *my_file = TFile::Open(simul_name.str().data());
         //TTree *tree_1e1a = (TTree*)my_file->Get("tree_rec_1e1a_source");
-        TTree *tree_1e1a = (TTree*)my_file->Get("tree_rec_1e1a_tracker");
-
+        //TTree *tree_1e1a = (TTree*)my_file->Get("tree_rec_1e1a_tracker");
+        TChain *tree_1e1a = new TChain("tree_rec_1e1a_source");
+        //TChain *tree_1e1a = new TChain("tree_rec_1e1a_tracker");
+        tree_1e1a->Add(simul_name.str().data());
+        
+        
         int n_geiger;
         double track_length, delta_t;
         
@@ -107,7 +112,7 @@ void BiPo() {
 
         sprintf(name,"fitted_actvity_%s",simul_names[i_file]);
 
-        h_fitted_activities[i_file] = new TH1D(name,name,1000,0,0.1);
+        h_fitted_activities[i_file] = new TH1D(name,name,500,activities[i_file]*0.2,activities[i_file]*1.8);
     }
     
     RooMsgService::instance().setGlobalKillBelow(RooFit::WARNING);
@@ -134,7 +139,11 @@ void BiPo() {
                                             track_length_obs, h_track_length[i_file]);
         pdfs[i_file] = new RooHistPdf(pdf_name.str().data(),pdf_name.str().data(),track_length_obs,
                                       *datahists[i_file],3);
-        fitted_activity[i_file] = new RooRealVar(var_name.str().data(),var_name.str().data(),activities[i_file]*given_exposure*0.,activities[i_file]*given_exposure*10.);
+        //fitted_activity[i_file] = new RooRealVar(var_name.str().data(),var_name.str().data(),activities[i_file]*given_exposure*0.,activities[i_file]*given_exposure*10.);
+        fitted_activity[i_file] = new RooRealVar(var_name.str().data(),var_name.str().data(),activities[i_file]*0.1*(given_exposure*efficiencies[i_file]),activities[i_file]*10.*(given_exposure*efficiencies[i_file]));
+        //fitted_activity[i_file] = new RooRealVar(var_name.str().data(),var_name.str().data(),activities[i_file]*(given_exposure*efficiencies[i_file]),"s-1");
+        fitted_activity[i_file]->setVal(activities[i_file]*(given_exposure*efficiencies[i_file]));
+
     }
     
     RooAddPdf model("model","model",RooArgSet(*pdfs[0],*pdfs[1],*pdfs[2]),RooArgList(*fitted_activity[0],*fitted_activity[1],*fitted_activity[2]));
@@ -162,28 +171,31 @@ void BiPo() {
             pl->SetTitle("");
             pl->Draw();
 
-            cout << fitted_activity[0]->getValV() << endl;
-            cout << fitted_activity[1]->getValV() << endl;
-            cout << fitted_activity[2]->getValV() << endl;
+            cout << activities[0]*(given_exposure*efficiencies[0]) <<" "<< fitted_activity[0]->getValV() << endl;
+            cout << activities[1]*(given_exposure*efficiencies[1]) <<" "<< fitted_activity[1]->getValV() << endl;
+            cout << activities[2]*(given_exposure*efficiencies[2]) <<" "<< fitted_activity[2]->getValV() << endl;
         }
     }
     
     //IMPORTANT: fitted_activity[0]->getValV() is equivalent to h_track_length[0]->Integral()*given_exposure
 
         
-    TCanvas *c4 = new TCanvas("track_length_models","track_length_models",1961,344,700,502);
+    TCanvas *c_fa[3];
+    for(int i_file=0;i_file<n_files;i_file++) {
+        sprintf(name,"fitted_activity_%s",simul_names[i_file]);
+        c_fa[i_file] = new TCanvas(name,name,1961,344,700,502);
 
-    h_fitted_activities[0]->SetLineWidth(2);
-    h_fitted_activities[0]->SetLineColor(2);
-    h_fitted_activities[0]->Draw();
+        h_fitted_activities[i_file]->SetLineWidth(2);
+        h_fitted_activities[i_file]->SetLineColor(i_file+2);
+        h_fitted_activities[i_file]->Draw();
 
-    h_fitted_activities[1]->SetLineWidth(2);
-    h_fitted_activities[1]->SetLineColor(3);
-    h_fitted_activities[1]->Draw("sames");
-
-    h_fitted_activities[2]->SetLineWidth(2);
-    h_fitted_activities[2]->SetLineColor(4);
-    h_fitted_activities[2]->Draw("sames");
+        TF1 *myfit = new TF1("myfit","gaus");
+        h_fitted_activities[i_file]->Fit(myfit);
+        cout << "FITTED HISTOGRAM OF MOCK-DATA ACTIVITIES WITH GAUSSIAN FUNCTION" << endl;
+        cout << "GAUSSIAN MEAN: " << myfit->GetParameter(1) << endl;
+        cout << "GAUSSIAN SIGMA: " << myfit->GetParameter(2) << endl;
+        cout << "ACCURACY OF ACTIVITY MEASUREMENT AFTER " << days_of_exposure << " DAYS: " << (myfit->GetParameter(2)/myfit->GetParameter(1))*100. << "%" <<endl;
+    }
 
     
     
